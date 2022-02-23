1e7e40ada4fb20504a384e5d10ab3d2649c23437
synodical <lena0323park@naver.com>
2022-02-23 15:23:59 +0900

    Feat: Favorite Button 만들고, 좋아요 개수 표시

--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Favorite(props) {
 
@@ -9,6 +9,9 @@ function Favorite(props) {
     const moviePost = props.movieInfo.backdrop_path
     const movieRunTime = props.movieInfo.runtime
 
+    const [FavoriteNumber, setFavoriteNumber] = useState(0)
+    const [Favorited, setFavorited] = useState(false)
+
     useEffect(() => {
         let variables = {
             userFrom,
@@ -16,17 +19,26 @@ function Favorite(props) {
         }
         Axios.post('/api/favorite/favoriteNumber', variables)
             .then(response => {
-                console.log(response.data)
+                setFavoriteNumber(response.data.favoriteNumber)
                 if (response.data.success) {
 
                 } else {
                     alert('좋아요 숫자 정보를 가져오는 데에 실패했습니다.');
                 }
             })
-    }, []);
+        Axios.post('/api/favorite/favorited', variables)
+            .then(response => {
+                if (response.data.success) {
+                    setFavorited(response.data.favorited)
+                } else {
+                    alert('정보를 가져오는데 실패 했습니다.')
+                }
+            })
+    }, [])
+
     return (
         <div>
-            <button>Favorite</button>
+            <button>{Favorited ? "Not Favorite" : "Add to Favorite"} {FavoriteNumber}</button>
         </div>
     )
 }
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -11,6 +11,19 @@ router.post('/favoriteNumber', (req, res) => {
             // 그다음에   프론트에  다시   숫자 정보를 보내주기  
             res.status(200).json({ success: true, favoriteNumber: info.length })
         })
-
 })
+router.post('/favorited', (req, res) => {
+
+    //mongoDB에서   favorite 숫자를 가져오기 
+    Favorite.find({ "movieId": req.body.movieId, "userFrom": req.body.userFrom })
+        .exec((err, info) => {
+            if (err) return res.status(400).send(err)
+            let result = false;
+            if (info.length !== 0) {
+                result = true
+            }
+            res.status(200).json({ success: true, favorited: result })
+        })
+})
+
 module.exports = router;
